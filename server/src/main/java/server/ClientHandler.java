767333469e2dfd753a032524a0be20bca78240ac
geetbrains <ylishnaf@yandex.ru>
2020-06-29 17:47:09 +0500

    hw4

--- server/src/main/java/server/ClientHandler.java
+++ server/src/main/java/server/ClientHandler.java
@@ -5,6 +5,9 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ClientHandler {
     private Server server;
@@ -16,137 +19,142 @@ public class ClientHandler {
     private String login;
 
     public ClientHandler(Server server, Socket socket) {
-        this.server = server;
-        this.socket = socket;
         try {
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
+            this.server = server;
+            this.socket = socket;
+            try {
+                in = new DataInputStream(socket.getInputStream());
+                out = new DataOutputStream(socket.getOutputStream());
+                ExecutorService service = Executors.newCachedThreadPool();
 
-            new Thread(() -> {
-                try {
-                    //Если в течении 5 секунд не будет сообщений по сокету то вызовится исключение
-                    socket.setSoTimeout(120000);
+                service.submit(() -> {
+                    try {
+                        //Если в течении 5 секунд не будет сообщений по сокету то вызовится исключение
+                        socket.setSoTimeout(120000);
 
-                    //цикл аутентификации
-                    while (true) {
-                        String str = in.readUTF();
+                        //цикл аутентификации
+                        while (true) {
+                            String str = in.readUTF();
 
-                        if (str.startsWith("/reg ")) {
-                            String[] token = str.split(" ");
+                            if (str.startsWith("/reg ")) {
+                                String[] token = str.split(" ");
 
-                            if (token.length < 4) {
-                                continue;
-                            }
-
-                            boolean succeed = server
-                                    .getAuthService()
-                                    .registration(token[1], token[2], token[3]);
-                            if (succeed) {
-                                sendMsg("Регистрация прошла успешно");
-                            } else {
-                                sendMsg("Регистрация  не удалась. \n" +
-                                        "Возможно логин уже занят, или данные содержат пробел");
-                            }
-                        }
-
-                        if (str.startsWith("/auth ")) {
-                            String[] token = str.split(" ");
-
-                            if (token.length < 3) {
-                                continue;
-                            }
-
-                            String newNick = server.getAuthService()
-                                    .getNicknameByLoginAndPassword(token[1], token[2]);
-
-                            login = token[1];
-
-                            if (newNick != null) {
-                                if (!server.isLoginAuthorized(login)) {
-                                    sendMsg("/authok " + newNick);
-                                    nick = newNick;
-                                    server.subscribe(this);
-                                    System.out.println("Клиент: " + nick + " подключился" + socket.getRemoteSocketAddress());
-                                    socket.setSoTimeout(0);
-
-                                    //==============//
-//                                    sendMsg(SQLHandler.getMessageForNick(nick));
-                                    //==============//
-
-                                    break;
-                                } else {
-                                    sendMsg("С этим логином уже прошли аутентификацию");
+                                if (token.length < 4) {
+                                    continue;
                                 }
-                            } else {
-                                sendMsg("Неверный логин / пароль");
-                            }
-                        }
-                    }
 
-                    //цикл работы
-                    while (true) {
-                        String str = in.readUTF();
-
-                        if (str.startsWith("/")) {
-                            if (str.equals("/end")) {
-                                sendMsg("/end");
-                                break;
+                                boolean succeed = server
+                                        .getAuthService()
+                                        .registration(token[1], token[2], token[3]);
+                                if (succeed) {
+                                    sendMsg("Регистрация прошла успешно");
+                                } else {
+                                    sendMsg("Регистрация  не удалась. \n" +
+                                            "Возможно логин уже занят, или данные содержат пробел");
+                                }
                             }
-                            if (str.startsWith("/w ")) {
-                                String[] token = str.split(" ", 3);
+
+                            if (str.startsWith("/auth ")) {
+                                String[] token = str.split(" ");
 
                                 if (token.length < 3) {
                                     continue;
                                 }
 
-                                server.privateMsg(this, token[1], token[2]);
-                            }
+                                String newNick = server.getAuthService()
+                                        .getNicknameByLoginAndPassword(token[1], token[2]);
 
-                            //==============//
-                            if (str.startsWith("/chnick ")) {
-                                String[] token = str.split(" ", 2);
-                                if (token.length < 2) {
-                                    continue;
-                                }
-                                if (token[1].contains(" ")) {
-                                    sendMsg("Ник не может содержать пробелов");
-                                    continue;
-                                }
-                                if (server.getAuthService().changeNick(this.nick, token[1])) {
-                                    sendMsg("/yournickis " + token[1]);
-                                    sendMsg("Ваш ник изменен на " + token[1]);
-                                    this.nick = token[1];
-                                    server.broadcastClientList();
+                                login = token[1];
+
+                                if (newNick != null) {
+                                    if (!server.isLoginAuthorized(login)) {
+                                        sendMsg("/authok " + newNick);
+                                        nick = newNick;
+                                        server.subscribe(this);
+                                        System.out.println("Клиент: " + nick + " подключился" + socket.getRemoteSocketAddress());
+                                        socket.setSoTimeout(0);
+
+                                        //==============//
+//                                    sendMsg(SQLHandler.getMessageForNick(nick));
+                                        //==============//
+
+                                        break;
+                                    } else {
+                                        sendMsg("С этим логином уже прошли аутентификацию");
+                                    }
                                 } else {
-                                    sendMsg("Не удалось изменить ник. Ник " + token[1] + " уже существует");
+                                    sendMsg("Неверный логин / пароль");
                                 }
                             }
-                            //==============//
+                        }
+
+                        //цикл работы
+                        while (true) {
+                            String str = in.readUTF();
+
+                            if (str.startsWith("/")) {
+                                if (str.equals("/end")) {
+                                    sendMsg("/end");
+                                    break;
+                                }
+                                if (str.startsWith("/w ")) {
+                                    String[] token = str.split(" ", 3);
+
+                                    if (token.length < 3) {
+                                        continue;
+                                    }
+
+                                    server.privateMsg(this, token[1], token[2]);
+                                }
+
+                                //==============//
+                                if (str.startsWith("/chnick ")) {
+                                    String[] token = str.split(" ", 2);
+                                    if (token.length < 2) {
+                                        continue;
+                                    }
+                                    if (token[1].contains(" ")) {
+                                        sendMsg("Ник не может содержать пробелов");
+                                        continue;
+                                    }
+                                    if (server.getAuthService().changeNick(this.nick, token[1])) {
+                                        sendMsg("/yournickis " + token[1]);
+                                        sendMsg("Ваш ник изменен на " + token[1]);
+                                        this.nick = token[1];
+                                        server.broadcastClientList();
+                                    } else {
+                                        sendMsg("Не удалось изменить ник. Ник " + token[1] + " уже существует");
+                                    }
+                                }
+                                //==============//
 
 
-                        } else {
-                            server.broadcastMsg(nick, str);
+                            } else {
+                                server.broadcastMsg(nick, str);
+                            }
+                        }
+                    } catch (SocketTimeoutException e) {
+                        sendMsg("/end");
+                    }
+                    ///////
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        server.unsubscribe(this);
+                        System.out.println("Клиент отключился");
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
                     }
-                } catch (SocketTimeoutException e) {
-                    sendMsg("/end");
-                }
-                ///////
-                catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    server.unsubscribe(this);
-                    System.out.println("Клиент отключился");
-                    try {
-                        socket.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -166,4 +174,6 @@ public class ClientHandler {
     public String getLogin() {
         return login;
     }
+
+
 }
